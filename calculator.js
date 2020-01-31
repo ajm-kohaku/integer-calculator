a93ac110cf6588956a3577c8732dd822a6d52af0
Amber Murphy <ajmurphy505@gmail.com>
2020-01-30 23:05:45 -0500

    getting most of the code together for the calculator

---
+++ calculator.js
@@ -0,0 +1,38 @@
+const calcField = document.querySelector('#calc-input');
+const precisionField = document.querySelector('#calc-precision');
+const submitButton = document.querySelector('.calc');
+const resetButton = document.querySelector('.reset');
+const formBody = document.querySelector('.calc-form');
+
+function validateCalc(event) {
+    var regex = new RegExp("^[0-9-%*\(\)\+\.\/]");
+    var key = String.fromCharCode(event.charCode ? event.which : event.charCode);
+    if (!regex.test(key)) {
+        event.preventDefault();
+        return false;
+    }
+}
+
+function validatePrecision(event) {
+    var regex = new RegExp("^[0-4]");
+    var key = String.fromCharCode(event.charCode ? event.which : event.charCode);
+    if (!regex.test(key)) {
+        event.preventDefault();
+        return false;
+    }
+}
+
+resetButton.addEventListener("click", (evt) => {
+    evt.preventDefault;
+    calcField.value = "";
+    precisionField.value = "";
+});
+
+submitButton.addEventListener('click', (evt) => {
+    evt.preventDefault;
+    calcField.value = eval(calcField.value);
+})
+
+$(".calc-form").submit((e) => {
+    e.preventDefault();
+});
